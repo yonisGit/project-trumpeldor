f8f00098e623221af142adec47647add03d74306
or <or512749@gmail.com>
2018-09-16 17:12:48 +0300

    layouts

---
+++ app/src/main/java/com/example/bguarts/camera_page.java
@@ -0,0 +1,13 @@
+package com.example.bguarts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class camera_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_page);
+    }
+}
